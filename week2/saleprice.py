45322f4027f1841a2d285d30afab6cc1d2658160
claudiacole96 <claudia.evans@bryanuniversity.edu>
2022-05-02 22:58:42 -0400

    week2 project

--- week2/saleprice.py
+++ week2/saleprice.py
@@ -4,22 +4,31 @@ itemQnt = int(input(f"How many {itemDescrip} are you purchasing today? "))
 
 itemPrice = float(input("What is the price of the product? "))
 
-fifteenOff = 15
-twentyFiveOff = 25
+fifteenOff = .15
+twentyFiveOff = .25
 
-def finalItemPrice(itemPrice):
+tax = .065
+
+def finalItemPrice():
     if itemPrice > 19.99:
-        return itemPrice * (100 - fifteenOff)
+        return itemPrice * (1 - fifteenOff)
     elif itemPrice > 39.99:
-        return itemPrice * (100 - twentyFiveOff)
+        return itemPrice * (1 - twentyFiveOff)
     else:
         return itemPrice
 
-def salesTax()
+def salesTax():
+    return (finalItemPrice() * itemQnt) * (tax)
+
+def totalPrice():
+    return (finalItemPrice() * itemQnt) * (tax + 1)
+
+def savedAmount():
+    return (itemPrice * itemQnt) - (finalItemPrice() * itemQnt)
 
 print("Your Receipt")
 print("---------------------------")
-print(f"{itemDescrip} priced at {finalItemPrice(itemPrice):,.2f}")
-print(f"Sales tax {salesTax:,.2f}")
-print(f"Total amount due: {totalPrice:,.2f}")
-print(f"You saved {savedAmount:,.2f}")
+print(f"{itemQnt} {itemDescrip} priced at {finalItemPrice():,.2f}")
+print(f"Sales tax {salesTax():,.2f}")
+print(f"Total amount due: {totalPrice():,.2f}")
+print(f"You saved {savedAmount():,.2f}")

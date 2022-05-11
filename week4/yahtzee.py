4f07e22f6a54f662cd67749877ff5496860ee61a
claudiacole96 <claudia.evans@bryanuniversity.edu>
2022-05-11 10:17:13 -0400

    .

--- week4/yahtzee.py
+++ week4/yahtzee.py
@@ -1,20 +1,3 @@
-"""
-Create a class for a die (single dice)
-    Define any necessary attributes.  Your die class should be able to
-        handle any number of sides such as 4, 6, 8, 10, 12, 20, 100, etc.
-    Create the following methods for the class:
-        __init__ - Constructor method
-        roll - Roll the die to get a new random value based on the
-            number of sides of the die
-        getCurrentFaceValue - Get the value currently showing on the die face
-        showDieFace - Display the value currently showing on the die. 
-Create a function that rolls five different dice.
-    Display the roll results to the screen
-    Checks to see if all five dice have the same face value.
-        If they do, then print “YAHTZEE” to the screen.
-Challenge: Display a die face to the screen instead of the numerical value of the die.  You can find images to use at 
-emojipedia.org
-"""
 import random
 class Die:
     def __init__(self, number, sides, faceValue):

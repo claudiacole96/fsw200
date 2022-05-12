740c9cd3b9efd71886328c5504c79243b4d95635
claudiacole96 <claudia.evans@bryanuniversity.edu>
2022-05-11 22:33:14 -0400

    file updates

--- week2/magic.py
+++ week2/magic.py
@@ -2,7 +2,7 @@ num1 = int(input("Pick any number! "))
 
 num2 = num1 * 3
 num3 = num2 + 6
-num4 = num3 / 3
+num4 = num3 // 3
 num5 = num4 - num1
 
 print(f"Input: {num1}")
---
+++ week5/users.py
@@ -0,0 +1,14 @@
+
+
+class User:
+    def __init__(self, fName, lName, email, uName, pWord, uuid, phone, cell, pictureLg, pictureTN):
+        self.fName = fName
+        self.lName = lName
+        self.email = email 
+        self.uName = uName 
+        self.pWord = pWord
+        self.uuid = uuid
+        self.phone = phone
+        self.cell = cell
+        self.pictureLg = pictureLg
+        self.pictureTN = pictureTN
